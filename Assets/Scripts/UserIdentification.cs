4b0bebf9262eed555c583907cdda089762bf632d
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2018-03-14 18:04:05 +0200

    User identification added

---
+++ Assets/Scripts/UserIdentification.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserIdentification : MonoBehaviour {
+
+    public InputField[] inputFields;
+    public GameObject textLog;
+    public SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        textLog.SetActive(false);
+    }
+
+    public bool IsInputEmpty()
+    {
+        foreach (InputField field in inputFields)
+        {
+            if (field.text.Length == 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void StartSimulation()
+    {
+        if (IsInputEmpty())
+        {
+            textLog.SetActive(true);
+        }
+        else
+        {
+            // if input is not empty load the lab scene.
+            sceneLoader.LoadScene("Lab");
+        }
+    }
+
+}
